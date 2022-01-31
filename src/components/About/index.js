b086008ca8a233b8f577f1333f78f48aa16f01c6
Matt Keys <MatthewjKeys22@gmail.com>
2022-01-31 15:06:59 -0700

    styling pretty much done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,24 +6,24 @@ return (
   
       <section id="about" className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-red">
             Hi, I'm Matt.
             <br className="hidden lg:inline-block" />I am a new full-stack developer.
           </h1>
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8 text-white leading-relaxed">
           I leave power, good. Thank you... thank you.  Taxes, they'll be lower. The democratic vote is the right thing to do Philadelphia...so do.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               onClick={() => setCurrentCategory('contact')}
-              className="inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-white bg-foam border-0 py-2 px-6 focus:outline-none hover:text-foam hover:bg-red rounded text-lg">
               Work With Me
             </a>
             <a
               href="#projects"
               onClick={() => setCurrentCategory('projects')}
-              className="ml-4 inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              className="ml-4 inline-flex text-white bg-foam border-0 py-2 px-6 focus:outline-none hover:bg-red hover:text-foam rounded text-lg">
               See My Past Work
             </a>
           </div>
